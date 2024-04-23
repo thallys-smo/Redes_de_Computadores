479857a1594d6813bfe2bdc64d6f08a54588c024
Thallys Oliveira <thallys.smo@gmail.com>
2024-04-23 15:39:22 -0300

    ...

--- Trabalho 1/src/main.cpp
+++ Trabalho 1/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+
+char msg = 'oi, aqui é a Kalinka, estou mandando uma mensagem!';
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
