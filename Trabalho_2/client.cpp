9d15a8c0dae2ee5da6df45e67a777673a385a729
MatheusDrm <matheus.drocca@usp.br>
2024-06-28 15:44:47 -0300

    Modularizando client e server scripts

--- Trabalho_2/client.cpp
+++ Trabalho_2/client.cpp
@@ -1,4 +1,3 @@
-// client.cpp
 #include <iostream>
 #include <cstring>
 #include <sys/types.h>
@@ -7,58 +6,112 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define PORT 8080
+// g++ -o client client.cpp
+// ./client
+
+int createSocket();
+void defineSocketAddr(struct sockaddr_in &server_addr, const std::string &ip, int port);
+void bindSocket(int socketFD, struct sockaddr_in &socket_addr, int server_port);
+void connectToSocket(int socketFD, struct sockaddr_in &server_addr); 
+int receiveData(int client_socketFD, char *buffer);
+std::string getUserInput(void);
+void sendData(int socketFD, const std::string &message);
 
 int main() {
-    int sock = 0;
+    int clientSocket = 0;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
 
     // Create socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return -1;
-    }
+    clientSocket = createSocket();
 
     // Define the server address
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
-        return -1;
-    }
+    int server_port = 8080;
+    defineSocketAddr(serv_addr, "127.0.0.1", server_port);
 
     // Connect to the server
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection Failed" << std::endl;
-        return -1;
-    }
+    connectToSocket(clientSocket, serv_addr);
 
     // Chat loop
     while (true) {
         // Get message from client user
-        std::cout << "Client: ";
-        std::string message;
-        std::getline(std::cin, message);
+        std::string message = getUserInput();
         
         // Send message to server
-        send(sock, message.c_str(), message.length(), 0);
+        sendData(clientSocket, message);
 
-        // Clear buffer
-        memset(buffer, 0, sizeof(buffer));
-        
         // Read reply from server
-        int valread = read(sock, buffer, 1024);
+        int valread = receiveData(clientSocket, buffer);
         if (valread <= 0) {
             std::cout << "Server disconnected or error occurred" << std::endl;
             break;
         }
-        std::cout << "Server: " << buffer << std::endl;
     }
 
     // Close the socket
-    close(sock);
+    close(clientSocket);
     return 0;
+}
+
+int createSocket() {
+    int server_socketFD;
+    
+    // Cria o socket file descriptor 
+    if ((server_socketFD = socket(AF_INET, SOCK_STREAM, 0)) == 0) { // SOCK_STREAM -> IPv4
+        perror("Criação do socket falhou");
+        exit(EXIT_FAILURE);
+    }
+
+    return server_socketFD;
+}
+
+void defineSocketAddr(struct sockaddr_in &server_addr, const std::string &ip, int port) {
+    // Define o endereço do servidor
+    server_addr.sin_family = AF_INET; // Especifica protocolo IPv4
+    server_addr.sin_port = htons(port); // Converte o número da porta fornecida para o formato aceito pela rede
+
+    // Se ip estiver definido como localHost o servidor irá ser capaz de ouvir em qualquer IP disponível
+    if (ip == "") {
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+    } else {
+        server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    }
+}
+
+void bindSocket(int socketFD, struct sockaddr_in &socket_addr, int server_port) {
+    if (bind(socketFD, (struct sockaddr *)&socket_addr, sizeof(socket_addr)) < 0) {
+        perror("Bind do servidor no endereço fornecido falhou");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    } else {
+        std::cout << "Bind estabelecidao com port " <<  server_port << std::endl;
+    }
+}
+
+void connectToSocket(int socketFD, struct sockaddr_in &server_addr) {
+    if (connect(socketFD, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+int receiveData(int client_socketFD, char *buffer) {
+    memset(buffer, 0, sizeof(buffer));
+    int recvData_len = recv(client_socketFD, buffer, sizeof(buffer), 0); 
+    if (recvData_len > 0) {
+        std::cout << "Servidor: " << buffer << std::endl;
+    }
+    return recvData_len;
+}
+
+std::string getUserInput(void) {
+    std::string clientMsg;
+    std::cout << "Cliente: ";
+    std::getline(std::cin, clientMsg);
+
+    return clientMsg;
+}
+
+void sendData(int socketFD, const std::string &message) {
+    send(socketFD, message.c_str(), message.length(), 0);
 }
--- Trabalho_2/server.cpp
+++ Trabalho_2/server.cpp
@@ -1,75 +1,143 @@
-// server.cpp
 #include <iostream>
 #include <cstring>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
 
-#define PORT 8080
+//g++ -o server server.cpp
+// ./server
+
+int createSocket();
+void defineSocketAddr(struct sockaddr_in &server_addr, const std::string &ip, int port);
+void bindSocket(int socketFD, struct sockaddr_in &socket_addr, int server_port);
+void socket_listenToConections(int socketFD, int maxConections);
+int socket_acceptConnection(int socketFD, struct sockaddr_in &client_addr, socklen_t &client_addrlen);
+int receiveData(int client_socketFD, char *buffer);
+std::string getUserInput(void);
+void sendData(int socketFD, const std::string &message);
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    int server_socketFD, new_socket;
+    struct sockaddr_in server_addr;
+    
+    // Cria o socket
+    server_socketFD = createSocket();
+
+    int server_port = 8080;
+    std::string server_IP = ""; // Se quiser usar o server ouça qualquer ip deixar vazio ""
+    if (server_IP == "localHost") {
+        server_IP = "127.0.0.1";
+    }
+
+    // Configura o endereço do socket
+    defineSocketAddr(server_addr, server_IP, server_port);
+    socklen_t server_addrlen = sizeof(server_addr);
+
+    // Bind o socket no endereço definido
+    bindSocket(server_socketFD, server_addr, server_port);
+
+    // Ouvindo conexões (limite de 10 conexões)
+    int maxConections = 10;
+    socket_listenToConections(server_socketFD, maxConections);
+
+    // Aceita novas conexões
+    struct sockaddr_in client_addr;
+    socklen_t client_addrlen = sizeof(client_addr);
+    new_socket = socket_acceptConnection(server_socketFD, client_addr, client_addrlen);
+
+
     char buffer[1024] = {0};
 
-    // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Define the server address
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Bind the socket to the network address and port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Accept a connection from a client
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Chat loop
+    // Loop de comunicação
     while (true) {
-        // Clear buffer
-        memset(buffer, 0, sizeof(buffer));
-        
-        // Read message from client
-        int valread = read(new_socket, buffer, 1024);
-        if (valread <= 0) {
-            std::cout << "Client disconnected or error occurred" << std::endl;
+        int recvData_len = receiveData(new_socket, buffer);
+        if (recvData_len <= 0) {
             break;
         }
-        std::cout << "Client: " << buffer << std::endl;
+        std::string serverMsg;
+        serverMsg = getUserInput();      
+        sendData(new_socket, serverMsg);
         
-        // Get reply message from server user
-        std::cout << "Server: ";
-        std::string reply;
-        std::getline(std::cin, reply);
-        
-        // Send reply to client
-        send(new_socket, reply.c_str(), reply.length(), 0);
     }
 
-    // Close the socket
+    // Fecha os sockets
     close(new_socket);
-    close(server_fd);
+    close(server_socketFD);
     return 0;
+}
+
+int createSocket() {
+    int server_socketFD;
+    
+    // Cria o socket file descriptor 
+    if ((server_socketFD = socket(AF_INET, SOCK_STREAM, 0)) == 0) { // SOCK_STREAM -> IPv4
+        perror("Criação do socket falhou");
+        exit(EXIT_FAILURE);
+    }
+
+    return server_socketFD;
+}
+
+void defineSocketAddr(struct sockaddr_in &server_addr, const std::string &ip, int port) {
+    // Define o endereço do servidor
+    server_addr.sin_family = AF_INET; // Especifica protocolo IPv4
+    server_addr.sin_port = htons(port); // Converte o número da porta fornecida para o formato aceito pela rede
+
+    // Se ip estiver definido como localHost o servidor irá ser capaz de ouvir em qualquer IP disponível
+    if(ip==""){
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+    }else{
+        server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    }
+}
+
+void bindSocket(int socketFD, struct sockaddr_in &socket_addr, int server_port) {
+    if (bind(socketFD, (struct sockaddr *)&socket_addr, sizeof(socket_addr)) < 0) {
+        perror("Bind do servidor no endereço fornecido falhou");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    }
+    else{
+        std::cout << "Bind estabelecidao com port " <<  server_port << std::endl;    }
+}
+
+void socket_listenToConections(int socketFD, int maxConections) {
+    if (listen(socketFD, maxConections) < 0) {
+        perror("Listen");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int socket_acceptConnection(int socketFD, struct sockaddr_in &client_addr, socklen_t &client_addrlen) {
+    int client_socket;
+
+    if ((client_socket = accept(socketFD, (struct sockaddr *)&client_addr, &client_addrlen)) < 0) {
+        perror("ERRO: Falha ao aceitar nova conexão");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    }
+    return client_socket;
+}
+
+int receiveData(int client_socketFD, char *buffer) {
+    memset(buffer, 0, sizeof(buffer));
+    int recvData_len = recv(client_socketFD, buffer, sizeof(buffer), 0); 
+    std::cout << "Cliente: " << buffer << std::endl;
+    return recvData_len;
+}
+
+std::string getUserInput(void){
+    std::string serverMsg;
+    std::cout << "Servidor: ";
+    std::getline(std::cin, serverMsg);
+
+    return serverMsg;
+}
+
+void sendData(int socketFD, const std::string &message) {
+    send(socketFD, message.c_str(), message.length(), 0);
 }
